ce33cda90333ffdb7b61da039ef2058fa76b9182
Thiago José Vieira de Souza Oliveira <thiago02_08@hotmail.com>
2020-08-09 18:56:53 -0300

    asdas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,14 +3,13 @@ import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
-import ButtonLink from './components/ButtonLink';
 // import ButtonLink from './components/ButtonLink';
 
 function Menu() {
   return (
     <nav className="Menu">
       <Link to="/">
-        <img className="Logo" src={Logo} alt="ThizoFlix logo" />
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
       </Link>
 
       <Button as={Link} className="ButtonLink" to="/cadastro/video">
